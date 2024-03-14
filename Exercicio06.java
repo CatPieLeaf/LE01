fe4e5d9bf832739d5d34ff0085df29b2d9dd235c
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 11:07:26 -0300

    Exercicio 6

--- Exercicio06.java
+++ Exercicio06.java
@@ -4,7 +4,10 @@ public class Exercicio06 {
     // Fahrenheit. A fórmula de conversão é:F = (9 * C + 160) / 5
 
     public static void run() {
-        
+        double c = Prompt.lerDecimal("Digite uma temperatura em Celsius:");
+        double f = (9 * c + 160) / 5;
+        Prompt.imprimir("Fahrenheit: " + f);
+
     }
 
 }
