a13bfb903b408a279243bfe5fb6ef04b784c5922
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 14:30:14 -0300

    Exercicio 10

--- Exercicio10.java
+++ Exercicio10.java
@@ -5,6 +5,13 @@ public class Exercicio10 {
     // inválido!”. 
 
     public static void run() {
+        int num = Prompt.lerInteiro("Digite um Número de 1 a 5:");
+        String[] extenso = new String[] {"um", "dois", "três", "quatro", "cinco"};
+        try {
+            Prompt.imprimir("Seu número: " + extenso[num-1]);
+        } catch (Exception e) {
+            Prompt.imprimir("Número inválido!");
+        }
     }
 
 }
