dc3354d6ab1c1061691a1aa0c2ef27d79ef8765c
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 16:01:03 -0300

    Exercicio 13

--- Exercicio13.java
+++ Exercicio13.java
@@ -6,6 +6,25 @@ public class Exercicio13 {
     // mensagem de operador não definido. Tratar erro de divisão por zero.
 
     public static void run() {
+        double a = Prompt.lerDecimal("Digite um Número A:");
+        double b = Prompt.lerDecimal("Digite um Número B:");
+        String caractere = Prompt.lerLinha("Digite um Operador Matemático:");
+        if (caractere.equals("+")) { 
+            Prompt.imprimir("Soma: " + (a+b));
+        } else if (caractere.equals("-")) { 
+            Prompt.imprimir("Subtraçao: " + (a-b));
+        } else if (caractere.equals("/")) {
+            if (b != 0) {
+                Prompt.imprimir("Divisao: " + (a/b));
+            } else {
+                Prompt.imprimir("Impossível dividir por zero!");
+            }
+        } else if (caractere.equals("*") || caractere.equals("x")) {
+            Prompt.imprimir("Multiplicaçao: " + (a*b));
+        } else if (caractere.equals("^")) {
+            Prompt.imprimir("Potenciaçao: " + (Math.pow(a, b)));
+        } else {
+            Prompt.imprimir("Operador nao definido!");
+        }
     }
-
 }
