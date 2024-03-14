dfc677606ddf3ecd10d411248545f49fd58923ae
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 11:24:01 -0300

    Exercicio 7

--- Exercicio07.java
+++ Exercicio07.java
@@ -5,6 +5,11 @@ public class Exercicio07 {
     // o usuário também deverá ser informado. 
 
     public static void run() {
+        int num = Prompt.lerInteiro("Digite um Número:");
+        if (100 < num && num < 200) {
+            Prompt.imprimir("O número está no intervalo entre 100 e 200");
+        } else {
+            Prompt.imprimir("O número é externo ao intervalo entre 100 e 200");
+        }
     }
-
 }
