728bd8caa18823d6068ac69979a1acdb99d2b7a2
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 14:57:04 -0300

    Exercicio 11

--- Exercicio11.java
+++ Exercicio11.java
@@ -1,9 +1,17 @@
+import java.util.Arrays;
+
 public class Exercicio11 {
 
     // Escreva um algoritmo que leia três valores inteiros distintos e escreva-os em
     // ordem crescente. 
 
     public static void run() {
+        int[] nums = new int[3];
+        for (int i = 0; i < 3; i++) {
+            nums[i] = Prompt.lerInteiro("Digite um Número:");
+        }
+        Arrays.sort(nums);
+        Prompt.imprimir("Em ordem crescente: " + Arrays.toString(nums).replace("[", "").replace("]", ""));
     }
 
 }
