6a4251c28c34b92efefd8ba98831edea6c9f9d45
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 10:26:47 -0300

    Exercício 2

--- Exercicio02.java
+++ Exercicio02.java
@@ -1,10 +1,11 @@
 public class Exercicio02 {
 
     // Escreva um algoritmo que leia dois números digitados 
-    // pelo usuário e exiba oresultado da sua soma
-
+    // pelo usuário e exiba o resultado da sua soma
 
     public static void run() {
+        int num1 = Prompt.lerInteiro("Digite um Número:");
+        int num2 = Prompt.lerInteiro("Digite mais um Número:");
+        Prompt.imprimir("Soma: " + (num1+num2));
     }
-
 }
