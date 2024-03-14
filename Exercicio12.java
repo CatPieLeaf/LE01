cbe7dbccf753d97f747cfbea009fab20e341f821
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 15:06:15 -0300

    Exercicio 12

--- Exercicio12.java
+++ Exercicio12.java
@@ -4,6 +4,13 @@ public class Exercicio12 {
     // correspondente. Valide mês inválido.
 
     public static void run() {
+        int num = Prompt.lerInteiro("Digite o número de um mês:");
+        String[] meses = new String[] {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        try {
+            Prompt.imprimir("Mês correspondente: " + meses[num-1]);
+        } catch (Exception e) {
+            Prompt.imprimir("Mês inválido!");
+        }
     }
 
 }
