40f134c8b277bb1a1fa80f0a66e8ba1db97036c7
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 10:23:00 -0300

    Exercício 1

--- Exercicio01.java
+++ Exercicio01.java
@@ -4,6 +4,12 @@ public class Exercicio01 {
     // caso este número seja maior, ou “Número menor ou igual a 10!” caso este número seja menor ou igual.
 
     public static void run() {
+        int num = Prompt.lerInteiro("Digite um Número");
+        if (num > 10) {
+            Prompt.imprimir("Número maior do que 10!");
+        } else {
+            Prompt.imprimir("Número menor ou igual a 10!");
+        }
     }
 
 }
