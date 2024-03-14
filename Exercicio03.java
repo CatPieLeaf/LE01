cf5290717b051ef8cff15c8ec071063f686e4b30
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 10:36:05 -0300

    Exercício 3

--- Exercicio03.java
+++ Exercicio03.java
@@ -4,8 +4,16 @@ public class Exercicio03 {
     // variáveis A e B e informe qual deles é o maior. Caso os números sejam iguais informarao usuário 
     // que a sequência de números informados é inválida
 
-
     public static void run() {
+        int a = Prompt.lerInteiro("Digite um Número A:");
+        int b = Prompt.lerInteiro("Digite um Número B:");
+        if (a == b) {
+            Prompt.imprimir("Sequência de Números Inválida!");
+        } else if (a > b) {
+            Prompt.imprimir("O Número A é maior que o número B.");
+        } else {
+            Prompt.imprimir("O Número B é maior que o número A.");
+        }
     }
 
 }
