edf9b7c07540bd672069a58e97faa9dcc7b6c98c
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-14 10:40:06 -0300

    Exercicio 4

--- Exercicio04.java
+++ Exercicio04.java
@@ -4,6 +4,11 @@ public class Exercicio04 {
     // multiplicação e a divisão dos números lidos.
 
     public static void run() {
+        int num1 = Prompt.lerInteiro("Digite um Número:");
+        int num2 = Prompt.lerInteiro("Digite mais um Número:");
+        Prompt.imprimir("Soma: " + (num1+num2));
+        Prompt.imprimir("Subtraçao: " + (num1-num2));
+        Prompt.imprimir("Multiplicaçao: " + (num1*num2));
+        Prompt.imprimir("Divisao: " + (num1/num2));
     }
-
 }
