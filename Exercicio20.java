ca15d7204641f5917c55678da21085fe66f486f8
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-15 10:05:23 -0300

    Exercicio 20

--- Exercicio20.java
+++ Exercicio20.java
@@ -4,9 +4,10 @@ public class Exercicio20 {
     // uma viagem, utilizando um automóvel que faz 12km por litro e considerando que são
     // fornecidos o tempo em hora e a velocidade média da viagem.
 
-
     public static void run() {
-
+        double t = Prompt.lerDecimal("Insira o tempo da viagem (horas):");
+        double v = Prompt.lerDecimal("Insira a velocidade média da viagem (km/h):");
+        double l = (t * v) / 12;
+        Prompt.imprimir("Litros gastos na viagem: " + l);
     }
-
 }
