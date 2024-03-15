66579923e98f4efac4958be63b37375e66368c43
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-15 09:47:38 -0300

    Exercicio 17

--- Exercicio17.java
+++ Exercicio17.java
@@ -10,7 +10,13 @@ public class Exercicio17 {
     // Média ponderada = 8.25
 
     public static void run() {
-
+        double nota1 = Prompt.lerDecimal("Digite nota 1 do aluno");
+        double nota2 = Prompt.lerDecimal("Digite nota 2 do aluno");
+        double nota3 = Prompt.lerDecimal("Digite nota 3 do aluno");
+        double peso1 = Prompt.lerDecimal("Digite o peso da nota 1");
+        double peso2 = Prompt.lerDecimal("Digite o peso da nota 2");
+        double peso3 = Prompt.lerDecimal("Digite o peso da nota 3");
+        double media = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / (peso1 + peso2 + peso3);
+        Prompt.imprimir("Média ponderada: " + media);
     }
-
 }
