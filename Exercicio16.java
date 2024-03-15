f1aae288c79131d64c7d5f480a21b88defb64d67
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-15 09:40:58 -0300

    Exercicio 16

--- Exercicio16.java
+++ Exercicio16.java
@@ -4,6 +4,10 @@ public class Exercicio16 {
     // aritmética. Exemplo: nota1 = 10.0, nota2 = 5.5, nota3 = 8.0. Média: 7.83
 
     public static void run() {
+        double nota1 = Prompt.lerDecimal("Digite nota 1 do aluno");
+        double nota2 = Prompt.lerDecimal("Digite nota 2 do aluno");
+        double nota3 = Prompt.lerDecimal("Digite nota 3 do aluno");
+        double media = (nota1 + nota2 + nota3) / 3;
+        Prompt.imprimir("Média: " + media);
     }
-
 }
