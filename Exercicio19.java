cf88ec578175ef262848e803fd72dd31b306aaf8
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-15 09:52:50 -0300

    Exercicio 19

--- Exercicio19.java
+++ Exercicio19.java
@@ -4,9 +4,10 @@ public class Exercicio19 {
     // volume calculado de acordo com a seguinte fórmula: volume = 3.14 * raio2 * altura;
     // Exemplo: raio = 10, altura = 15. Volume = 4710
 
-
     public static void run() {
-
+        double raio = Prompt.lerDecimal("Insira o raio do cilindro");
+        double altura = Prompt.lerDecimal("Insira a altura do cilindro");
+        double volume = 3.14 * Math.pow(raio, 2) * altura;
+        Prompt.imprimir("Volume: " + volume);
     }
-
 }
